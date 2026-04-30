4543638a125bd26e98d77f964191584541dc2960
Joseph Khoury <joseph-khoury@outlook.com>
2026-04-30 14:33:51 +0300

    chore: add prettier rules, update app assets, clean boiler plate code

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path')
 
 const buildExpoLintCommand = (filenames) =>
   `expo lint --fix ${filenames
     .map((f) => `"${path.relative(process.cwd(), f)}"`)
-    .join(" ")}`;
+    .join(' ')}`
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildExpoLintCommand, "prettier --write"],
-  "!(*.{js,jsx,ts,tsx})": ["prettier --write --ignore-unknown"],
-};
+  '*.{js,jsx,ts,tsx}': [buildExpoLintCommand, 'prettier --write'],
+  '!(*.{js,jsx,ts,tsx})': ['prettier --write --ignore-unknown'],
+}
